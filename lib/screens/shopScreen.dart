179aa9e105c7e918e56303922896c54ae9f79e30
Alex <alexmolina@gmail.com>
2020-08-29 15:20:20 +0200

    addBar añadida

--- lib/screens/shopScreen.dart
+++ lib/screens/shopScreen.dart
@@ -7,7 +7,16 @@ class shopScreen extends StatefulWidget {
   _shopScreenState createState() => _shopScreenState();
 }
 
-class _shopScreenState extends State<shopScreen> {
+class _shopScreenState extends State<shopScreen> with SingleTickerProviderStateMixin{
+
+  TabController _tabController;
+
+  @override
+  void initState(){
+    super.initState();
+    _tabController = TabController(initialIndex: 0, length: 5 , vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,8 +33,44 @@ class _shopScreenState extends State<shopScreen> {
                   Icon(Icons.shopping_cart , size: 30.0 , color: Colors.black)
                 ],)
             ),
+            SizedBox(height: 20.0),
+            Padding(
+                padding: EdgeInsets.only(left: 30.0) ,
+                child: Text(
+                  'Lo más comprado',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600
+                  ),
+                )
+            ),
+            SizedBox(height: 20.0),
+            TabBar(
+              controller: _tabController,
+              indicatorColor: Colors.transparent,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.withOpacity(0.6),
+              labelPadding: EdgeInsets.symmetric(horizontal: 35.0),
+              isScrollable: true,
+              tabs: [
+                Tab(
+                    child: Text('Top' , style: TextStyle(fontSize: 16.0 , fontWeight: FontWeight.bold),)
+                ),
+                Tab(
+                    child: Text('Top' , style: TextStyle(fontSize: 16.0 , fontWeight: FontWeight.bold),)
+                ),
+                Tab(
+                    child: Text('Top' , style: TextStyle(fontSize: 16.0 , fontWeight: FontWeight.bold),)
+                ),
+                Tab(
+                    child: Text('Top' , style: TextStyle(fontSize: 16.0 , fontWeight: FontWeight.bold),)
+                ),
+                Tab(
+                    child: Text('Top' , style: TextStyle(fontSize: 16.0 , fontWeight: FontWeight.bold),)
+                )
+              ],
+            )
 
-            SizedBox(height: 20.0,)
 
           ],
       ),),
