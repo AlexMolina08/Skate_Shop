1a757fd7dfd20eb4d71beaa3dfa2f1b7266212b7
Alex <alexmolina@gmail.com>
2020-08-30 12:00:19 +0200

    añadida skateScreen y Gesture detector en las tarjetas de SkateShop

--- lib/screens/shopScreen.dart
+++ lib/screens/shopScreen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:skate_shop/models/skate_model.dart';
+import 'package:skate_shop/screens/skateScreen.dart';
 
 class shopScreen extends StatefulWidget {
   @override
@@ -40,78 +41,81 @@ class _shopScreenState extends State<shopScreen> with SingleTickerProviderStateM
           height: Curves.easeInOut.transform(value) * 500,
           width: Curves.easeInOut.transform(value) * 400,
           child: widget,
-        )
-
-        );
+        ));
       },
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Container(decoration: BoxDecoration(
-            color: Color(0x5F0080ff),
-            borderRadius: BorderRadius.circular(20.0)
-          ),
-            margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 30.0),
-            child: Stack(children:[
-              Center(child: Hero(
-                tag: skates[index].imageUrl,
-                child: Image(
-                  height: 280,
-                  width: 280,
-                  image: AssetImage(
-                    'assets/images/skate$index.png'
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(context,MaterialPageRoute(builder: (_) => SkateScreen(skate: skates[index])));
+        },
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(decoration: BoxDecoration(
+              color: Color(0x5F0080ff),
+              borderRadius: BorderRadius.circular(20.0)
+            ),
+              margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 30.0),
+              child: Stack(children:[
+                Center(child: Hero(
+                  tag: skates[index].imageUrl,
+                  child: Image(
+                    height: 280,
+                    width: 280,
+                    image: AssetImage(
+                      'assets/images/skate$index.png'
+                    ),
+                    //fit: BoxFit.cover
                   ),
-                  //fit: BoxFit.cover
+                ),),
+                Positioned(
+                  top: 30.0 ,
+                  right: 30.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                          "DESDE",
+                          style: TextStyle(color:Colors.white, fontSize: 15.0)),
+                      Text(
+                          "\ ${skates[index].price} €",
+                          style: TextStyle(color:Colors.white, fontSize: 25.0 , fontWeight: FontWeight.w600))
+                    ],
+                  )
                 ),
-              ),),
-              Positioned(
-                top: 30.0 ,
-                right: 30.0,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                        "DESDE",
+                Positioned(
+                  left: 15.0,
+                  bottom: 20.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        skates[index].category.toUpperCase(),
                         style: TextStyle(color:Colors.white, fontSize: 15.0)),
-                    Text(
-                        "\ ${skates[index].price} €",
-                        style: TextStyle(color:Colors.white, fontSize: 25.0 , fontWeight: FontWeight.w600))
-                  ],
+                      SizedBox(height: 1.0),
+                      Text(
+                       skates[index].name,
+                        style: TextStyle(color:Colors.white, fontSize: 25.0, fontWeight: FontWeight.w500),
+                      )
+                    ],
+                  )
                 )
-              ),
-              Positioned(
-                left: 15.0,
-                bottom: 20.0,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      skates[index].category.toUpperCase(),
-                      style: TextStyle(color:Colors.white, fontSize: 15.0)),
-                    SizedBox(height: 1.0),
-                    Text(
-                     skates[index].name,
-                      style: TextStyle(color:Colors.white, fontSize: 25.0, fontWeight: FontWeight.w500),
-                    )
-                  ],
-                )
+              ]),
+            ),
+
+            Positioned(
+              bottom: 4.0,
+              child: RawMaterialButton(
+                padding:EdgeInsets.all(15.0),
+                shape: CircleBorder(),
+                elevation: 2.0,
+                fillColor: Colors.black,
+                child: Icon(Icons.add_shopping_cart , color: Colors.white , size:30.0),
+                onPressed: ()=> print("añadido al carrito"),
               )
-            ]),
-          ),
-
-          Positioned(
-            bottom: 4.0,
-            child: RawMaterialButton(
-              padding:EdgeInsets.all(15.0),
-              shape: CircleBorder(),
-              elevation: 2.0,
-              fillColor: Colors.black,
-              child: Icon(Icons.add_shopping_cart , color: Colors.white , size:30.0),
-              onPressed: ()=> print("añadido al carrito"),
             )
-          )
 
-        ]
+          ]
+        ),
       ),
     );
   }
@@ -193,7 +197,11 @@ class _shopScreenState extends State<shopScreen> with SingleTickerProviderStateM
                 children: [
                   Text('Descripción' , style: TextStyle(fontSize: 22.0 , fontWeight: FontWeight.w600),),
                   SizedBox(height: 10.0,),
-
+                  Text(skates[_selectedPage].description ,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black87
+                    ),)
                 ],
               )
             )
---
+++ lib/screens/skateScreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:skate_shop/models/skate_model.dart';
+
+class SkateScreen extends StatefulWidget {
+
+  final Skate skate;
+
+  SkateScreen({this.skate}  );
+
+  @override
+  _SkateScreenState createState() => _SkateScreenState();
+}
+
+class _SkateScreenState extends State<SkateScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
