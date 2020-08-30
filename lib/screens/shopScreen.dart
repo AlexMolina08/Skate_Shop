5538319444699a127dd30657c9b98eb55420abd6
Alex <alexmolina@gmail.com>
2020-08-30 11:48:44 +0200

    metodo _selectSkate terminado

--- lib/screens/shopScreen.dart
+++ lib/screens/shopScreen.dart
@@ -27,73 +27,92 @@ class _shopScreenState extends State<shopScreen> with SingleTickerProviderStateM
   * Crea una carta de cada skate para la lista
   * */
   _skateSelector(int index){
-    return Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        Container(decoration: BoxDecoration(
-          color: Color(0x5F0080ff),
-          borderRadius: BorderRadius.circular(20.0)
-        ),
-          margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 30.0),
-          child: Stack(children:[
-            Center(child: Hero(
-              tag: skates[index].imageUrl,
-              child: Image(
-                height: 280,
-                width: 280,
-                image: AssetImage(
-                  'assets/images/skate$index.png'
-                ),
-                //fit: BoxFit.cover
-              ),
-            ),),
-            Positioned(
-              top: 30.0 ,
-              right: 30.0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                      "DESDE",
-                      style: TextStyle(color:Colors.white, fontSize: 15.0)),
-                  Text(
-                      "\ ${skates[index].price} €",
-                      style: TextStyle(color:Colors.white, fontSize: 25.0 , fontWeight: FontWeight.w600))
-                ],
-              )
-            ),
-            Positioned(
-              left: 15.0,
-              bottom: 20.0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    skates[index].category.toUpperCase(),
-                    style: TextStyle(color:Colors.white, fontSize: 15.0)),
-                  SizedBox(height: 1.0),
-                  Text(
-                   skates[index].name,
-                    style: TextStyle(color:Colors.white, fontSize: 25.0, fontWeight: FontWeight.w500),
-                  )
-                ],
-              )
-            )
-          ]),
-        ),
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (BuildContext context , Widget widget){
+        double value = 1;
+        if(_pageController.position.haveDimensions){
+          value = _pageController.page - index;
+          value = (1 - (value.abs() * 0.3)).clamp(0.0, 1.0);
+        }
 
-        Positioned(
-          bottom: 4.0,
-          child: RawMaterialButton(
-            padding:EdgeInsets.all(15.0),
-            shape: CircleBorder(),
-            elevation: 2.0,
-            fillColor: Colors.black,
-            child: Icon(Icons.add_shopping_cart , color: Colors.white)
-          )
+        return Center(child:SizedBox(
+          height: Curves.easeInOut.transform(value) * 500,
+          width: Curves.easeInOut.transform(value) * 400,
+          child: widget,
         )
 
-      ]
+        );
+      },
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(decoration: BoxDecoration(
+            color: Color(0x5F0080ff),
+            borderRadius: BorderRadius.circular(20.0)
+          ),
+            margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 30.0),
+            child: Stack(children:[
+              Center(child: Hero(
+                tag: skates[index].imageUrl,
+                child: Image(
+                  height: 280,
+                  width: 280,
+                  image: AssetImage(
+                    'assets/images/skate$index.png'
+                  ),
+                  //fit: BoxFit.cover
+                ),
+              ),),
+              Positioned(
+                top: 30.0 ,
+                right: 30.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                        "DESDE",
+                        style: TextStyle(color:Colors.white, fontSize: 15.0)),
+                    Text(
+                        "\ ${skates[index].price} €",
+                        style: TextStyle(color:Colors.white, fontSize: 25.0 , fontWeight: FontWeight.w600))
+                  ],
+                )
+              ),
+              Positioned(
+                left: 15.0,
+                bottom: 20.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      skates[index].category.toUpperCase(),
+                      style: TextStyle(color:Colors.white, fontSize: 15.0)),
+                    SizedBox(height: 1.0),
+                    Text(
+                     skates[index].name,
+                      style: TextStyle(color:Colors.white, fontSize: 25.0, fontWeight: FontWeight.w500),
+                    )
+                  ],
+                )
+              )
+            ]),
+          ),
+
+          Positioned(
+            bottom: 4.0,
+            child: RawMaterialButton(
+              padding:EdgeInsets.all(15.0),
+              shape: CircleBorder(),
+              elevation: 2.0,
+              fillColor: Colors.black,
+              child: Icon(Icons.add_shopping_cart , color: Colors.white , size:30.0),
+              onPressed: ()=> print("añadido al carrito"),
+            )
+          )
+
+        ]
+      ),
     );
   }
 
@@ -165,8 +184,19 @@ class _shopScreenState extends State<shopScreen> with SingleTickerProviderStateM
                   return _skateSelector(index);
                 },
               )
-            )
+            ),
 
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Descripción' , style: TextStyle(fontSize: 22.0 , fontWeight: FontWeight.w600),),
+                  SizedBox(height: 10.0,),
+
+                ],
+              )
+            )
 
           ],
       ),),
